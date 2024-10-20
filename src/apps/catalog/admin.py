4652a5b9dd47ff4bf2a8426086f00c89d5f31a22
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-10-20 07:29:18 +0330

    create model category

--- src/apps/catalog/admin.py
+++ src/apps/catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
 
-# Register your models here.
+from apps.catalog.models import Category
+
+
+class CategoryAdmin(TreeAdmin):
+    form = movenodeform_factory(Category)
+
+
+admin.site.register(Category, CategoryAdmin)
--- src/apps/catalog/models.py
+++ src/apps/catalog/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 
+from treebeard.mp_tree import MP_Node
+
+
+class Category(MP_Node):
+    title = models.CharField(max_length=100, db_index=True)
+    description = models.CharField(max_length=100, null=True, blank=True)
+    public = models.BooleanField(default=False)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
